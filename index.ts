0edce308b9c5dcb69badb0590b244db190a956eb
jmoguilevsky <jmoguilevsky@gmail.com>
2022-03-10 16:02:31 -0300

    add bastion keypair

--- index.ts
+++ index.ts
@@ -42,16 +42,23 @@ export = async function main() {
     ],
   });
 
+  const keypair = new aws.ec2.KeyPair("bastion", {
+    publicKey:
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCxAJ7s/FJnvasStDeq391PYHKi2iTrM9GB+XY0NJp7TII310LpGcikaj1IkrhmitdXZK0wrSfQalhviBWMnp0ebQilv+6IEZaTALVeAzZoHZEO83/zsqe7nXyfLUuT7OL6/COy9N2hie9/2yj9beYEZlnSwBi0N9qMr3vglJopCfxOj8RFzw7hq2qJMtjgTN9HKERHekov5N3JHsWkS8YfCpo9FCBqo3fXh3NZ4Ekkd8xsj+8wN4AlulVl8/5HWaTZ16ZTFBvj7j/aqFITx1t3ools2x450r/oCaB/+J+d0zmvc76iItS2FBOiOpby9IF0HVe9LNAavw6Gsov/DVUr",
+    keyName: "bastion",
+  })
+
+
   const ec2 = new aws.ec2.Instance(
     "synapse-testing-instance",
     {
       ami,
       instanceType: "t2.micro" as pulumi.Input<aws.ec2.InstanceType>,
       associatePublicIpAddress: true,
+      keyName: "bastion",
 
       // gracefully stop the machine to not corrupt any data
       instanceInitiatedShutdownBehavior: "stop",
-
       vpcSecurityGroupIds: [securityGroup.id],
     },
     {
