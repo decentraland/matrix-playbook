16c75e354fae34f4a63d1b6d61bb558c9daa85d7
jmoguilevsky <jmoguilevsky@gmail.com>
2022-03-10 16:13:00 -0300

    remove duplicate keypair

--- index.ts
+++ index.ts
@@ -1,14 +1,14 @@
-import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
-import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws"
+import * as awsx from "@pulumi/awsx"
+import * as pulumi from "@pulumi/pulumi"
 
-import { getAmi } from "dcl-ops-lib/getAmi";
-import { getPublicBastionIp } from "dcl-ops-lib/supra";
+import { getAmi } from "dcl-ops-lib/getAmi"
+import { getPublicBastionIp } from "dcl-ops-lib/supra"
 
 export = async function main() {
-  const ami = getAmi("ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20200112", { owners: ["099720109477"] });
+  const ami = getAmi("ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20200112", { owners: ["099720109477"] })
 
-  const vpc = awsx.ec2.Vpc.getDefault();
+  const vpc = awsx.ec2.Vpc.getDefault()
   const securityGroup = new aws.ec2.SecurityGroup("synapse-test-security", {
     vpcId: vpc.id,
     egress: [
@@ -40,14 +40,7 @@ export = async function main() {
         protocol: "udp",
       },
     ],
-  });
-
-  const keypair = new aws.ec2.KeyPair("bastion", {
-    publicKey:
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCxAJ7s/FJnvasStDeq391PYHKi2iTrM9GB+XY0NJp7TII310LpGcikaj1IkrhmitdXZK0wrSfQalhviBWMnp0ebQilv+6IEZaTALVeAzZoHZEO83/zsqe7nXyfLUuT7OL6/COy9N2hie9/2yj9beYEZlnSwBi0N9qMr3vglJopCfxOj8RFzw7hq2qJMtjgTN9HKERHekov5N3JHsWkS8YfCpo9FCBqo3fXh3NZ4Ekkd8xsj+8wN4AlulVl8/5HWaTZ16ZTFBvj7j/aqFITx1t3ools2x450r/oCaB/+J+d0zmvc76iItS2FBOiOpby9IF0HVe9LNAavw6Gsov/DVUr",
-    keyName: "bastion",
   })
-
 
   const ec2 = new aws.ec2.Instance(
     "synapse-testing-instance",
@@ -66,8 +59,7 @@ export = async function main() {
       protect: false,
       dependsOn: [],
     }
-  );
+  )
 
   return ec2
-
 }
