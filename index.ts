eafdca71e30065e9ea65ccec48d45eb630aa7a0d
jmoguilevsky <jmoguilevsky@gmail.com>
2022-03-31 10:55:04 -0300

    added userdata

--- index.ts
+++ index.ts
@@ -6,12 +6,19 @@ import * as cloudflare from "@pulumi/cloudflare"
 import { getAmi } from "dcl-ops-lib/getAmi"
 import { getPublicBastionIp } from "dcl-ops-lib/supra"
 import { setRecord, getZoneId } from "dcl-ops-lib/cloudflare"
+import { interpolate } from "@pulumi/pulumi"
 
 export = async function main() {
   const ami = getAmi("ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-20200112", { owners: ["099720109477"] })
 
   const vpc = awsx.ec2.Vpc.getDefault()
   const stackName = pulumi.getStack()
+
+  const userData = interpolate`#!/bin/bash
+  set +x
+
+  echo "Cloning synapse repository..."
+  git clone https://dcl.tools/ops/synapse-test-server.git`
 
   const securityGroup = new aws.ec2.SecurityGroup("synapse-test-security", {
     vpcId: vpc.id,
@@ -58,6 +65,7 @@ export = async function main() {
       // gracefully stop the machine to not corrupt any data
       instanceInitiatedShutdownBehavior: "stop",
       vpcSecurityGroupIds: [securityGroup.id],
+      userData,
     },
     {
       deleteBeforeReplace: true,
