55271bf0ad6f693b100704d15a9e3f4d37bcfc7a
guidota <guido@decentraland.org>
2022-04-28 16:36:32 -0300

    feat: add subdomain parameter to templates (#6)

--- generate-inventory.sh
+++ generate-inventory.sh
@@ -1,16 +1,30 @@
+
 # Check env file exists
-test -e .env || echo '.env file does not exist' && exit
+echo "Checking .env file..."
+if [[ ! -f .env ]]; then
+    echo ".env file does not exist"
+    exit
+fi
 
 # Read and export env vars from .env
-export $(grep -v '^#' .env | xargs -d '\n')
+echo "Loading env vars from .env file..."
+export $(grep -v '^#' .env | xargs)
+
+# Set subdomain default if not set
+: "${SUBDOMAIN:=synapse}"
 
 # Create inventory hosts file
-sed -e "s/MATRIX_DOMAIN/$MATRIX_DOMAIN/g" -e "s/PRIVATE_IP/${PRIVATE_IP}/g" -e "s/SSH_USER/$SSH_USER/g" templates/hosts > inventory/hosts
+echo "Creating inventory hosts file..."
+sed -e "s/MATRIX_DOMAIN/$MATRIX_DOMAIN/g" -e "s/PRIVATE_IP/${PRIVATE_IP}/g" -e "s/SSH_USER/$SSH_USER/g" -e "s/SUBDOMAIN/$SUBDOMAIN/g" templates/hosts > inventory/hosts
 
 # Create inventory host vars.yml
-OUTPUT_FOLDER=inventory/host_vars/matrix.$MATRIX_DOMAIN/
+OUTPUT_FOLDER=inventory/host_vars/$SUBDOMAIN.$MATRIX_DOMAIN/
 mkdir -p $OUTPUT_FOLDER
-sed -e "s/MATRIX_DOMAIN/$MATRIX_DOMAIN/g" -e "s/GENERIC_SECRET_KEY/$GENERIC_SECRET_KEY/g" -e "s/POSTGRES_PASSWORD/$POSTGRES_PASSWORD/g" templates/vars.yml > $OUTPUT_FOLDER/vars.yml
+sed -e "s/MATRIX_DOMAIN/$MATRIX_DOMAIN/g" -e "s/GENERIC_SECRET_KEY/$GENERIC_SECRET_KEY/g" -e "s/POSTGRES_PASSWORD/$POSTGRES_PASSWORD/g" -e "s/SUBDOMAIN/$SUBDOMAIN/g" templates/vars.yml > $OUTPUT_FOLDER/vars.yml
+echo "Generated vars.yaml moved to: $OUTPUT_FOLDER"
 
 # Copy auth provider plugin
+echo "Password auth provider moved to: $OUTPUT_FOLDER"
 cp templates/decentraland_password_auth_provider.py $OUTPUT_FOLDER
+
+echo "Done!"
