382745a12c4693f9e370245f1a4aa73a9757b0da
guidota <guido@tamborindeguy.com.ar>
2022-04-29 18:49:47 -0300

    feat: use jinja templates for inventory generation

--- generate-inventory.sh
+++ generate-inventory.sh
@@ -1,27 +1,30 @@
+#!/bin/bash
 
 # Check env file exists
 echo "Checking .env file..."
-if [[ ! -f .env ]]; then
-    echo ".env file does not exist"
-    exit
-fi
+test -e .env || (echo '.env file does not exist' && exit)
 
 # Read and export env vars from .env
 echo "Loading env vars from .env file..."
-export $(grep -v '^#' .env | xargs)
+source .env
 
-# Set subdomain default if not set
-: "${SUBDOMAIN:=synapse}"
+pip3 install j2cli
+
+# # Check if docker compose is installed
+# if ! [ -x "$(command -v j2)" ]; then
+#   echo -n "Error: j2 is not installed..." >&2
+#   exit 1
+# fi
 
 # Create inventory hosts file
 echo "Creating inventory hosts file..."
-sed -e "s/MATRIX_DOMAIN/$MATRIX_DOMAIN/g" -e "s/PRIVATE_IP/${PRIVATE_IP}/g" -e "s/SSH_USER/$SSH_USER/g" -e "s/SUBDOMAIN/$SUBDOMAIN/g" templates/hosts > inventory/hosts
+j2 --format=env templates/hosts.j2 .env > inventory/hosts
 
 # Create inventory host vars.yml
 OUTPUT_FOLDER=inventory/host_vars/$SUBDOMAIN.$MATRIX_DOMAIN/
 mkdir -p $OUTPUT_FOLDER
-sed -e "s/MATRIX_DOMAIN/$MATRIX_DOMAIN/g" -e "s/GENERIC_SECRET_KEY/$GENERIC_SECRET_KEY/g" -e "s/POSTGRES_PASSWORD/$POSTGRES_PASSWORD/g" -e "s/SUBDOMAIN/$SUBDOMAIN/g" templates/vars.yml > $OUTPUT_FOLDER/vars.yml
 echo "Generated vars.yaml moved to: $OUTPUT_FOLDER"
+j2 --format=env templates/vars.yml.j2 .env > $OUTPUT_FOLDER/vars.yml
 
 # Copy auth provider plugin
 echo "Password auth provider moved to: $OUTPUT_FOLDER"
